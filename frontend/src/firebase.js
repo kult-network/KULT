e28be15823cee957ee67ec17939929a017ad0dcf
Tanvi <support.kult@gmail.com>
2026-04-07 17:13:37 +0530

    Last changes

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,18 +1,25 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCu-UbJIIkHJpJVqA_zWZfPxyMdVm3JRcs",
-  authDomain: "kult-e9a2a.firebaseapp.com",
+  
+  // ⚠️ THE FIX: Change this from firebaseapp.com to your Vercel URL
+  authDomain: "kult-nine.vercel.app", 
+  
   projectId: "kult-e9a2a",
   storageBucket: "kult-e9a2a.firebasestorage.app",
   messagingSenderId: "1080960407908",
   appId: "1:1080960407908:web:c85eb3b0d807cf21708271",
   measurementId: "G-9L1MNVBNJV"
 };
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
 export const googleProvider = new GoogleAuthProvider();
 googleProvider.setCustomParameters({
   prompt: 'select_account'
 });
+
 export default app;
