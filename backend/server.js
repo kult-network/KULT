b0d9c6d7f1a3fd1d34d54c3a11907bc8b5cd7fc7
Tanvi <support.kult@gmail.com>
2026-04-15 23:55:59 +0530

    fix(mail): add accessToken to EmailJS request for strict mode support

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,7 @@ const sendEmail = async (to, subject, htmlContent) => {
             service_id: process.env.EMAILJS_SERVICE_ID,
             template_id: process.env.EMAILJS_TEMPLATE_ID,
             user_id: process.env.EMAILJS_PUBLIC_KEY,
+            accessToken: process.env.EMAILJS_PRIVATE_KEY, // Forces authorization in Strict Mode
             template_params: {
                 to_email: to,
                 subject: subject,
